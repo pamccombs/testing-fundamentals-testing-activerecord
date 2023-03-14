c35727a93569f4d8ececd0b2c2e39b1d2fa1c34c
P. Reese McCombs <34119196+pamccombs@users.noreply.github.com>
2023-03-14 18:27:07 +0000

    Test cannot be completed

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -3,7 +3,9 @@ require "spec_helper"
 describe Person do
   it "requires a name to be valid" do
     # Write your spec here.
+    #Write a spec to ensure that the model is invalid without a `first_name`.
+    person = Person.new(first_name: nil)
+    expect(person).not_to be_valid
     # You can delete the pending line.
-    pending "Not written yet"
   end
 end
